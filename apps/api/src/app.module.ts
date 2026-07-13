09c06013e2a039e727794d308c05363d8628f27f
Khushal <Khushallinkedin11@gmail.com>
2026-07-14 01:09:58 +0530

    Backend connection and users CRUD

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+
+import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    PrismaModule,
+    UsersModule,
+  ],
 })
 export class AppModule {}
---
+++ apps/api/src/modules/users/dto/create-user.dto.ts
@@ -0,0 +1,6 @@
+export class CreateUserDto {
+  name: string;
+  email: string;
+  avatar?: string;
+  githubId?: string;
+}
